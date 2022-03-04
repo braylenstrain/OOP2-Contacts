5ec47fbfcbf1ef6761cbc08b896e6a63ce3954d0
Braylen Strain <76047181+braylenstrain@users.noreply.github.com>
2022-03-04 03:34:11 -0700

    Add button action to switch to DisplayContacts

--- ContactsOOP2.java
+++ ContactsOOP2.java
@@ -1,3 +1,4 @@
+package application;
 /*
  * Author: Braylen Strain
  * Date: 03/03/2022
@@ -17,11 +18,25 @@ public class ContactsOOP2 extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Scene scene = new Scene(new Homepage(), 600, 350);
+		
+		//Load all the different pages
+		Homepage homepage = new Homepage();
+		DisplayContacts displayContacts = new DisplayContacts();
+		
+		//Set the scene to homepage when program starts
+		Scene scene = new Scene(homepage, 600, 350);
 		primaryStage.setTitle("Contacts: Homepage");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		primaryStage.setResizable(false);
+		
+		//Set actions for homepage
+		homepage.btDisplay.setOnAction(e -> {
+			Scene scene2 = new Scene(displayContacts, 800, 550);
+			primaryStage.setTitle("Contacts: Display Contacts");
+			primaryStage.setScene(scene2);
+			primaryStage.show();
+		});
 	}
 
 }
