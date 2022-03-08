9f44d0c440495948c078bbca644881e58095bf72
Braylen Strain <76047181+braylenstrain@users.noreply.github.com>
2022-03-07 17:37:44 -0700

    Add contacts by pressing enter, Add sortContacts method

--- ContactsOOP2.java
+++ ContactsOOP2.java
@@ -1,4 +1,3 @@
-package application;
 /*
  * Author: Braylen Strain
  * Date: 03/03/2022
@@ -10,11 +9,12 @@ import java.util.ArrayList;
 import javafx.stage.Stage;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.text.Text;
 
 public class ContactsOOP2 extends Application{
 	
-	ArrayList<Contact> contacts = new ArrayList<>();
+	static ArrayList<Contact> contacts = new ArrayList<>();
 
 	public static void main(String[] args) {
 		launch(args);
@@ -58,30 +58,40 @@ public class ContactsOOP2 extends Application{
 			primaryStage.setTitle("Contacts: Add Contacts");
 			primaryStage.setScene(scene3);
 			
-			//Take text from all text fields and add a Contact to contacts when btAdd is clicked
-			addContacts.btAdd.setOnAction(f -> {
-				Contact contact = new Contact();
-				contact.setFirstName(addContacts.tfFirstName.getText().trim());
-				contact.setLastName(addContacts.tfLastName.getText().trim());
-				contact.setPersonalPhoneNumber(addContacts.tfPersonalPhone.getText().trim());
-				contact.setWorkPhoneNumber(addContacts.tfWorkPhone.getText().trim());
-				contact.setPersonalEmailAddress(addContacts.tfPersonalEmail.getText().trim());
-				contact.setWorkEmailAddress(addContacts.tfWorkEmail.getText().trim());
-				contacts.add(contact);
-				
-				//Display how many contacts have been added at the top of the scene
-				addContacts.addSuccessful();
-				System.out.println(contacts.get(contacts.size() - 1));
-				
-				//Clear the text fields
-				addContacts.tfFirstName.clear();
-				addContacts.tfLastName.clear();
-				addContacts.tfPersonalPhone.clear();
-				addContacts.tfWorkPhone.clear();
-				addContacts.tfPersonalEmail.clear();
-				addContacts.tfWorkEmail.clear();
+			//When btAdd is clicked, take text from all text fields and add a Contact to contacts
+			addContacts.btAdd.setOnAction(f -> addContacts.addContact());
+			
+			//When the enter key is pressed, Take text from all text fields and add a Contact to contacts
+			addContacts.setOnKeyPressed(f -> {
+				if (f.getCode() == KeyCode.ENTER)
+						addContacts.addContact();
+			});
+			
+			addContacts.btExit.setOnAction(f -> {
+				sortContacts();
+				primaryStage.setTitle("Contacts: Homepage");
+				primaryStage.setScene(scene);
 			});
 		});
+	}
+	
+	//Sort the contacts ArrayList by Contact first name
+	public static void sortContacts() {
+		for (int i = 0; i < contacts.size() - 1; i++) {
+			Contact temp = contacts.get(i);
+			int tempIndex = i;
+			for (int j = i + 1; j < contacts.size(); j++) {
+				if (temp.compareTo(contacts.get(j)) == -1) {
+					temp = contacts.get(j);
+					tempIndex = j;
+				}
+			}
+			if (!temp.equals(contacts.get(i))) {
+				contacts.set(tempIndex, contacts.get(i));
+				contacts.set(i, temp);
+				System.out.println(contacts);
+			}
+		}
 	}
 
 }
