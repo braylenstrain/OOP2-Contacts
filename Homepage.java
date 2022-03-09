e245bf2696f7735148cdbd34289f768d8e1f4158
404kevnotfound <76047181+braylenstrain@users.noreply.github.com>
2022-03-08 19:44:23 -0700

    Merge delete and modify buttons, adjust button sizes

--- Homepage.java
+++ Homepage.java
@@ -10,20 +10,18 @@ public class Homepage extends BorderPane {
 	VBox fileOrExit = new VBox(50);
 	Button btDisplay = new Button("Display Contacts");
 	Button btAdd = new Button("Add Contacts");
-	Button btDelete = new Button("Delete Contacts");
-	Button btModify = new Button("Modify Contacts");
+	Button btDeleteModify = new Button("Delete/Modify Contacts");
 	Button btSearch = new Button("Search Contacts");
 	Button btExit = new Button("Exit Program");
 	Button btFile = new Button("Load Contacts");
 	
 	public Homepage() {
 		//Create Layout for VBox options
-		btDisplay.setPrefSize(300, 50);
-		btAdd.setPrefSize(300, 50);
-		btDelete.setPrefSize(300, 50);
-		btModify.setPrefSize(300, 50);
-		btSearch.setPrefSize(300, 50);
-		options.getChildren().addAll(btDisplay, btAdd, btDelete, btModify, btSearch, btExit, btFile);
+		btDisplay.setPrefSize(300, 65);
+		btAdd.setPrefSize(300, 65);
+		btDeleteModify.setPrefSize(300, 65);
+		btSearch.setPrefSize(300, 65);
+		options.getChildren().addAll(btDisplay, btAdd, btDeleteModify, btSearch, btExit, btFile);
 		
 		//Create layout for VBox fileOrExit
 		btExit.setPrefSize(200, 130);
